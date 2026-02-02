b1af971b79160979320a4865e238dcaacf69b624
jwr1 <47087725+jwr1@users.noreply.github.com>
2026-02-02 09:53:52 -0500

    Remove linows webview implementation

--- flutter_web_auth_2/lib/src/linows.dart
+++ flutter_web_auth_2/lib/src/linows.dart
@@ -2,12 +2,10 @@ import 'dart:async';
 
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
 import 'package:flutter_web_auth_2/src/server.dart';
-import 'package:flutter_web_auth_2/src/webview.dart';
 import 'package:flutter_web_auth_2_platform_interface/flutter_web_auth_2_platform_interface.dart';
 
 /// Implements the plugin interface for Linux and Windows (Linows).
 class FlutterWebAuth2LinowsPlugin extends FlutterWebAuth2Platform {
-  final FlutterWebAuth2Platform _webviewImpl = FlutterWebAuth2WebViewPlugin();
   final FlutterWebAuth2Platform _serverImpl = FlutterWebAuth2ServerPlugin();
 
   /// Registers the Linows super-implementation.
@@ -22,13 +20,6 @@ class FlutterWebAuth2LinowsPlugin extends FlutterWebAuth2Platform {
     required Map<String, dynamic> options,
   }) async {
     final parsedOptions = FlutterWebAuth2Options.fromJson(options);
-    if (parsedOptions.useWebview) {
-      return _webviewImpl.authenticate(
-        url: url,
-        callbackUrlScheme: callbackUrlScheme,
-        options: options,
-      );
-    }
     return _serverImpl.authenticate(
       url: url,
       callbackUrlScheme: callbackUrlScheme,
@@ -39,6 +30,5 @@ class FlutterWebAuth2LinowsPlugin extends FlutterWebAuth2Platform {
   @override
   Future clearAllDanglingCalls() async {
     await _serverImpl.clearAllDanglingCalls();
-    await _webviewImpl.clearAllDanglingCalls();
   }
 }
